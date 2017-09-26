8ccd5fe0ca183ab9c1ff1dfbde0ae36efb5fddd3
Tony Kraus <tonykraus@wireless-nat-inside.uiowa.edu>
2017-09-26 16:25:28 -0500

    Added some features to Packet Assembler

--- Java/Packet Assembler/packetAssembly.java
+++ Java/Packet Assembler/packetAssembly.java
@@ -12,28 +12,57 @@ public class packetAssembly {
       Scanner in = new Scanner(System.in);
       String str = in.nextLine();
       String[] line;
-      int mID, pID, pSize;
+      Double mID, pID;
+      Integer pSize;
       String message;
-      Map<Integer, String> out = new TreeMap<>();
-      while (!(str.equals(""))) {
+      Map<Double, String> out = new TreeMap<>();
+      Map<Double, Integer> mStatus = new TreeMap<>();
+      while (!str.equals("")) {
         line = str.split("\\s+");
-        mID = Integer.parseInt(line[0]);
-        pID = Integer.parseInt(line[1]);
+        mID = Double.parseDouble(line[0]);
+        pID = Double.parseDouble(line[1]);
         pSize = Integer.parseInt(line[2]);
         message = String.join(" ", (Arrays.copyOfRange(line, 3, line.length)));
-        out.put(pID + mID, message);
+        Integer status = mStatus.get(mID);
+        if (status != null) {
+          mStatus.put(mID, status-1);
+        }else {
+          mStatus.put(mID, pSize-1);
+        }
+        if (pID != 0) {
+          out.put(mID + (pID/100), message);
+        }else {
+          out.put(mID, message);
+        }
         str = in.nextLine();
       }
       in.close();
       PrintWriter outFile = new PrintWriter(new FileWriter(new File("output.txt")));
-      for (Map.Entry<Integer, String> x : out.entrySet()) {
-        outFile.println(x.getValue());
+      for (Map.Entry<Double, String> entry : out.entrySet()) {
+        double mplusp = Math.floor(entry.getKey());
+        String strout = entry.getValue();
+        Integer mstat = mStatus.get(mplusp);
+        if (mstat != null) {
+          if (mstat == 0) {
+            strout = String.format("Message %d:\n", (long)mplusp) + str;
+            mStatus.remove(mplusp);
+          }else if (mstat == 1) {
+            strout = String.format("Message %d (missing a packet):\n", (long)mplusp) + str;
+            mStatus.remove(mplusp);
+          }else {
+            strout = String.format("Message %d (missing %d packets):\n", (long)mplusp, mstat) + str;
+            mStatus.remove(mplusp);
+          }
+        }
+        outFile.println(strout);
       }
       outFile.close();
     }catch (IOException e) {
 			System.out.println(e.getMessage());
 		}catch (InputMismatchException e) {
 			System.out.println(e.getMessage());
-		}
+		}catch (NumberFormatException e) {
+      System.out.println("Packets must be entered in the following line format:\nmessage ID | packet ID | packets/message | message data");
+    }
   }
 }
