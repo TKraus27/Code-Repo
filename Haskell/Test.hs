8e17efd61731bed2c45417ea3032a1a3edb4c71f
TKraus27 <tvk270@gmail.com>
2020-06-02 01:05:28 -0500

    organizing and updating things

--- Haskell/Test.hs
+++
@@ -1,7 +0,0 @@
-type Ctordecl = (String {- ctor name -}, [String] {- ctor input types -})
-
-dataTypeFromNames :: String {- the name of the Hawkid datatype -} -> [Ctordecl] -> String
-dataTypeFromNames n cs = "something"
-
-example :: [Ctordecl]
-example = [("Zero",[]) , ("Suc",["Nat"])]
